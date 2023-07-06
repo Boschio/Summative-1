4d113d8580f6d374996c81dbb7c66832fb56945d
DAVID BISSOU <davidebissou@gmail.com>
2023-07-05 23:10:13 -0500

    Summative-1:Added Magic8BallController and Models

--- src/main/java/com/company/Summative1/controller/Magic8BallController.java
+++ src/main/java/com/company/Summative1/controller/Magic8BallController.java
@@ -1,20 +1,34 @@
 package com.company.Summative1.controller;
 
-import com.company.Summative1.models.Magic8Ball;
+import com.company.Summative1.models.Answer;
+import com.company.Summative1.models.Question;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class Magic8BallController {
 
+    List<String> responses = List.of("Truly wonderful, the mind of a child is.",
+            "The greatest teacher, failure is.",
+            "Judge me by my size, do you?",
+            "When nine hundred years old you reach, look as good you will not.",
+            "Attachment leads to jealousy. The shadow of greed, that is.",
+            "Beware…anger, fear, aggression. The Dark Side are they.",
+            "Wars not make one great.",
+            "That is why you fail.",
+            "Fear is the path to the dark side...fear leads to anger...anger leads to hate...hate leads to suffering.",
+            "Do or do not. There is no try.");
+
     @RequestMapping(value = "/magic", method = RequestMethod.POST)
     @ResponseStatus(value = HttpStatus.OK)
-    public Magic8Ball getMagic(String question) {
-
-        return new Magic8Ball();
+    @ResponseBody
+    public Answer getMagicAnswer(@RequestBody Question question) {
+        int index = (int) (Math.random() * responses.size());
+        String answer = responses.get(index);
+        Answer response = new Answer(index, question.getQuestion(), answer);
+        return response;
     }
 
 }
--- src/main/java/com/company/Summative1/models/Answer.java
+++ src/main/java/com/company/Summative1/models/Answer.java
@@ -1,2 +1,39 @@
-package com.company.Summative1.models;public class Answer {
+package com.company.Summative1.models;
+
+public class Answer {
+    private int id;
+    private String question;
+    private String answer;
+
+    public Answer(){}
+
+    public Answer(int id, String question, String answer){
+        this.question = question;
+        this.id = id;
+        this.answer = answer;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
 }
